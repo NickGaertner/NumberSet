a43808189eb7f0306f42e2727ee1da800f5adaf0
Nick Gärtner <nick.gaertner@pm.me>
2023-02-26 21:06:32 +0100

    deleted everything from doc branch

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A small library to perform set operations on number sets
- * that can be represented by a finite amount of intervals.
- *
- * @packageDocumentation
- */
-export * from './Interval';
-export * from './PredefinedIntervals';
-export * from './NumberSet';
